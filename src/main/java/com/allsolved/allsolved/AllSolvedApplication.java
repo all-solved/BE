5d1b1258f51456047c39b69b11bff11c9e514c63
Darren4641 <74559561+Darren4641@users.noreply.github.com>
2023-03-16 18:44:37 +0900

    init project

---
+++ src/main/java/com/allsolved/allsolved/AllSolvedApplication.java
@@ -0,0 +1,13 @@
+package com.allsolved.allsolved;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AllSolvedApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AllSolvedApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/allsolved/allsolved/AllSolvedApplicationTests.java
@@ -0,0 +1,13 @@
+package com.allsolved.allsolved;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AllSolvedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
